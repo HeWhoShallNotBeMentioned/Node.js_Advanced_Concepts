cd474115d8eafd68deebadd9b355f7b1255987a7
Chris Underwood <cunderwoodmn@gmail.com>
2020-08-24 18:53:45 -0400

    begin coding the pseudo code for the event loop.

--- loop.js
+++ loop.js
@@ -1,3 +1,12 @@
 // pseudo code for what the Node.js event loop looks like
 
 // node myFile.js
+
+myFile.runContents();
+
+function shouldContinue() {}
+
+// Each running of body in while loop executing is called a tick
+while (shouldContinue()) {}
+
+// exit back to terminal
