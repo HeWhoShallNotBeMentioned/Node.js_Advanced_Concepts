3dcb767dd8dbafb44a713ded760f88a3511082ba
Chris Underwood <cunderwoodmn@gmail.com>
2020-08-24 21:32:01 -0400

    filled out should continue conditions of for returning to the command line.

--- loop.js
+++ loop.js
@@ -2,9 +2,21 @@
 
 // node myFile.js
 
+const pendingTimers = [];
+const pendingOSTasks = [];
+const pendingOperations = [];
+
 myFile.runContents();
 
-function shouldContinue() {}
+function shouldContinue() {
+  // Check 1: Any pending setTimeout, setInterval, setImmediate?
+  // Check 2: Pending OS tasks? (Like server listening to port)
+  // Check 3: Any pending long running operations? (Like fs module)
+
+  return (
+    pendingTimers.length || pendingOSTasks.length || pendingOperations.length
+  );
+}
 
 // Each running of body in while loop executing is called a tick
 while (shouldContinue()) {}
