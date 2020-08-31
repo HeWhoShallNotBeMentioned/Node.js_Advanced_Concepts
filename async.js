03333793bd298d65e925a81f321d1bd607aa3246
Chris Underwood <cunderwoodmn@gmail.com>
2020-08-30 21:08:14 -0400

    converted https to a helper function that was called 7 times to show that pendingOSTasks is not using the node.js threads like pendingOperations.

--- async.js
+++ async.js
@@ -2,11 +2,21 @@ const https = require('https');
 
 const start = Date.now();
 
-https
-  .request('https://google.com', res => {
-    res.on('data', () => {});
-    res.on('end', () => {
-      console.log(Date.now() - start);
-    });
-  })
-  .end();
+function doRequest(num) {
+  https
+    .request('https://google.com', res => {
+      res.on('data', () => {});
+      res.on('end', () => {
+        console.log(Date.now() - start, 'num---   ', num);
+      });
+    })
+    .end();
+}
+
+doRequest(1);
+doRequest(2);
+doRequest(3);
+doRequest(4);
+doRequest(5);
+doRequest(6);
+doRequest(7);
