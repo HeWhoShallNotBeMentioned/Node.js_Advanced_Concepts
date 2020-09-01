286f5944a11d6c5552bf6d806c9578e15ca8ed5f
Chris Underwood <cunderwoodmn@gmail.com>
2020-09-01 19:50:42 -0400

    put in notes to describe what is happening in this page.

--- multitask.js
+++ multitask.js
@@ -1,3 +1,15 @@
+process.env.UV_THREADPOOL_SIZE = 4;
+
+// Without the process.env the default threadpool size is 4.
+// The 1st action returned will probably be doRequest because https operates outside the threadpool.
+// Next will be the 1st Hash to the bcrypt function then the FS, and finally the other 3 hashes.
+// This is because while the FS is called before the hashes it has a couple of async steps that allow the hashes to bump in.
+// the fs slips in when a threads becomes available from the hashes completing. This fs is fast as the file is small and local not on a network.
+// with 1 thread the fs is the last completed action.
+// with 2 threeds the fis is the 2nd to last completed action.
+// with 5 threads fs has its own thread and completes first.
+// The https is not guarenteed to operate with the same speed relatice to the other servies as it is going via the internet and requiring a response from google.
+
 const https = require('https');
 const crypto = require('crypto');
 const fs = require('fs');
