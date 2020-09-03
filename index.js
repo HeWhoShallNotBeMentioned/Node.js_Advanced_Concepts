b467ef4750c0c36ed1e77e335634422cbc8558d5
Chris Underwood <cunderwoodmn@gmail.com>
2020-09-02 22:30:38 -0400

    add doWork to the event loop to show that if 2 browsers are trying to use the same single event loop server, the 2nd will have to wait for the 1st to complete.

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const app = express();
+
+function doWork(duration) {
+  const start = Date.now();
+  while (Date.now() - start < duration) {}
+}
+
 app.get('/', (req, res) => {
-  res.send('Hi there');
+  doWork(5000);
+  res.send('Hi there ---------------------');
 });
 
 app.listen(4500);
+
+//Having this app run in 2 different browsers with single threading holds up the 2nd while the first completes.
