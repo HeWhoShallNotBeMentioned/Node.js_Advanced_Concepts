2586522f2b53090a453982cc137a6fc2084873e3
Chris Underwood <cunderwoodmn@gmail.com>
2020-09-04 13:48:51 -0400

    add starter code for clustering.

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-const express = require('express');
-const app = express();
+const cluster = require('cluster');
 
-function doWork(duration) {
-  const start = Date.now();
-  while (Date.now() - start < duration) {}
+// Is the file being executes in master mode?
+if (cluster.isMaster) {
+  console.log('cluster.isMaster ----  ', cluster.isMaster);
+  // Cause index.js to be executed *again* but in child mode
+  cluster.fork();
+} else {
+  // I'm a child, I'm going to act like va server and do nothing else
+  console.log('forked ----  ');
+  const express = require('express');
+  const app = express();
+
+  function doWork(duration) {
+    const start = Date.now();
+    while (Date.now() - start < duration) {}
+  }
+
+  app.get('/', (req, res) => {
+    doWork(5000);
+    res.send('Hi there ---------------------');
+  });
+
+  app.listen(4500);
 }
-
-app.get('/', (req, res) => {
-  doWork(5000);
-  res.send('Hi there ---------------------');
-});
-
-app.listen(4500);
-
 //Having this app run in 2 different browsers with single threading holds up the 2nd while the first completes.
