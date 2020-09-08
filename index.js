545be178ce7fae333c33f71f6668c5f10fd208ec
Chris Underwood <cunderwoodmn@gmail.com>
2020-09-07 21:39:23 -0400

    formatted index.js to use a simple worker thread. Moved previous index.js to index_with_clustering_testing_code.js

--- index.js
+++ index.js
@@ -2,11 +2,29 @@
 const express = require('express');
 const crypto = require('crypto');
 const app = express();
+const Worker = require('worker_threads').Worker;
 
 app.get('/', (req, res) => {
-  crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
-    res.send('Hi there ---------------------');
+  // note the lack of using => arrow functions
+  const worker = new Worker(function () {
+    // When the app sends a message to the worker this is invoked
+    this.onmessage = function () {
+      let counter = 0;
+      while (counter < 1e9) {
+        counter++;
+      }
+      // When the worker sends a message to the app this is invoked
+      postMessage(counter);
+    };
   });
+
+  // when our worker sends a message back to the application, onmessage is called.
+  worker.onmessage = function (myCounter) {
+    console.log('worker.onmessage ----  ', myCounter);
+  };
+
+  // Send a message to our thread.
+  worker.postMessage();
 });
 
 app.get('/fast', (req, res) => {
