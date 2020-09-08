0f8bfda166fcc84e07786411dac67a9f6162f800
Chris Underwood <cunderwoodmn@gmail.com>
2020-09-07 22:27:23 -0400

    rewrote with help of other students the webworker code. It works when running a basic test, but not when using the benchmark testing tool.

--- index.js
+++ index.js
@@ -1,30 +1,24 @@
 // I'm a child, I'm going to act like va server and do nothing else
 const express = require('express');
-const crypto = require('crypto');
+//const crypto = require('crypto');
 const app = express();
-const Worker = require('worker_threads').Worker;
+const { Worker } = require('worker_threads');
 
 app.get('/', (req, res) => {
   // note the lack of using => arrow functions
-  const worker = new Worker(function () {
-    // When the app sends a message to the worker this is invoked
-    this.onmessage = function () {
-      let counter = 0;
-      while (counter < 1e9) {
-        counter++;
-      }
-      // When the worker sends a message to the app this is invoked
-      postMessage(counter);
-    };
+  const worker = new Worker('./worker.js');
+  worker.on('message', data => {
+    console.log(data);
+    res.send(' ' + data);
   });
 
-  // when our worker sends a message back to the application, onmessage is called.
-  worker.onmessage = function (myCounter) {
-    console.log('worker.onmessage ----  ', myCounter);
-  };
+  //   // when our worker sends a message back to the application, onmessage is called.
+  //   worker.onmessage = function (myCounter) {
+  //     console.log('worker.onmessage ----  ', myCounter);
+  //   };
 
-  // Send a message to our thread.
-  worker.postMessage();
+  //   // Send a message to our thread.
+  //   worker.postMessage();
 });
 
 app.get('/fast', (req, res) => {
