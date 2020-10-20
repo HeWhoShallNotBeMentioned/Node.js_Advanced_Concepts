f125f39222cc3c3c7d5d76d7645e3bf4d0538c86
Chris Underwood <cunderwoodmn@gmail.com>
2020-10-20 19:48:10 -0400

    add Redis client to /api/blogs route and set up get request for any blogs for the loggedin user from the redis store.

--- AdvancedNodeStarter/routes/blogRoutes.js
+++ AdvancedNodeStarter/routes/blogRoutes.js
@@ -7,13 +7,26 @@ module.exports = app => {
   app.get('/api/blogs/:id', requireLogin, async (req, res) => {
     const blog = await Blog.findOne({
       _user: req.user.id,
-      _id: req.params.id
+      _id: req.params.id,
     });
 
     res.send(blog);
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
+    const redis = require('redis');
+    const redisUrl = 'redis://127.0.0.1:6379';
+    const client = redis.createClient(redisUrl);
+    const util = require('util');
+    client.get = util.promisify(client.get);
+
+    // Do we have any data in redis related to this query
+    const cashedBlogs = await client.get(req.user.id);
+
+    // If yes, then respond to the request right away and return
+
+    // If no, retrieve data from Mongoose and update our redis store
+
     const blogs = await Blog.find({ _user: req.user.id });
 
     res.send(blogs);
@@ -25,7 +38,7 @@ module.exports = app => {
     const blog = new Blog({
       title,
       content,
-      _user: req.user.id
+      _user: req.user.id,
     });
 
     try {
