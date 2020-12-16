93943e7def12decf7f75e72182639a4e9149d6b9
Chris Underwood <cunderwoodmn@gmail.com>
2020-12-16 13:07:38 -0800

    reset the blogs.get route to remove the redis configuration that was tested there.

--- AdvancedNodeStarter/routes/blogRoutes.js
+++ AdvancedNodeStarter/routes/blogRoutes.js
@@ -14,7 +14,31 @@ module.exports = app => {
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
-    const redis = require('redis');
+    const blogs = await Blog.find({ _user: req.user.id });
+
+    res.send(blogs);
+  });
+
+  app.post('/api/blogs', requireLogin, async (req, res) => {
+    const { title, content } = req.body;
+
+    const blog = new Blog({
+      title,
+      content,
+      _user: req.user.id,
+    });
+
+    try {
+      await blog.save();
+      res.send(blog);
+    } catch (err) {
+      res.send(400, err);
+    }
+  });
+};
+
+/*
+const redis = require('redis');
     const redisUrl = 'redis://127.0.0.1:6379';
     const client = redis.createClient(redisUrl);
     const util = require('util');
@@ -36,22 +60,4 @@ module.exports = app => {
     console.log('SERVING FROM MONGODB');
     res.send(blogs);
     client.set(req.user.id, JSON.stringify(blogs));
-  });
-
-  app.post('/api/blogs', requireLogin, async (req, res) => {
-    const { title, content } = req.body;
-
-    const blog = new Blog({
-      title,
-      content,
-      _user: req.user.id,
-    });
-
-    try {
-      await blog.save();
-      res.send(blog);
-    } catch (err) {
-      res.send(400, err);
-    }
-  });
-};
+    */
