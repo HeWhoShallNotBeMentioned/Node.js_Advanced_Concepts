dc9c215bebefcfe43c14c6c62401256e105e2d11
Chris Underwood <cunderwoodmn@gmail.com>
2020-12-16 17:17:17 -0800

    created the framework for redis keys.

--- AdvancedNodeStarter/services/cache.js
+++ AdvancedNodeStarter/services/cache.js
@@ -4,7 +4,11 @@ const exec = mongoose.Query.prototype.exec;
 
 mongoose.Query.prototype.exec = function () {
   console.log("I'M about to run a function.");
-  console.log('this.getQuery in cache.js---', this.getFilter());
-  console.log('collection name---', this.mongooseCollection.name);
+
+  const key = Object.assign({}, this.getFilter(), {
+    collection: this.mongooseCollection.name,
+  });
+
+  console.log('key---', key);
   return exec.apply(this, arguments);
 };
