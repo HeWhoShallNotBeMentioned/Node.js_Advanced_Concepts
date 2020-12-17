af2206560f61d4419ca0ec3c05b07ff0e89760cd
Chris Underwood <cunderwoodmn@gmail.com>
2020-12-16 17:45:48 -0800

    put redis config into cache.js

--- AdvancedNodeStarter/services/cache.js
+++ AdvancedNodeStarter/services/cache.js
@@ -1,4 +1,9 @@
 const mongoose = require('mongoose');
+const redis = require('redis');
+const redisUrl = 'redis://127.0.0.1:6379';
+const client = redis.createClient(redisUrl);
+const util = require('util');
+client.get = util.promisify(client.get);
 
 const exec = mongoose.Query.prototype.exec;
 
