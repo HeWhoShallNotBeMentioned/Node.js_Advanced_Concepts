7a160f8c1825f911c97bb48f66c2c1d347857b4b
Chris Underwood <cunderwoodmn@gmail.com>
2020-12-16 16:16:24 -0800

    found where we will grab the data for creating keys.

--- AdvancedNodeStarter/services/cache.js
+++ AdvancedNodeStarter/services/cache.js
@@ -4,5 +4,7 @@ const exec = mongoose.Query.prototype.exec;
 
 mongoose.Query.prototype.exec = function () {
   console.log("I'M about to run a function.");
+  console.log('this.getQuery in cache.js---', this.getFilter());
+  console.log('collection name---', this.mongooseCollection.name);
   return exec.apply(this, arguments);
 };
