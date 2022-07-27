e356fdcce526ced5ef7ec060ee5cb1ac841a07b1
Chris Underwood <cunderwoodmn@gmail.com>
2022-07-27 15:09:03 -0700

    add dev.js to gitignore and stop tracking to key our keys private.

--- AdvancedNodeStarter/config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  googleClientID:
-    '594118917988-pn4roulqmu4338js8h36igdcc4u1g7dk.apps.googleusercontent.com',
-  googleClientSecret: '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',
-  mongoURI:
-    'mongodb+srv://Chris123:BeckettSmithsonMelanie@cluster0.y76h5.mongodb.net/?retryWrites=true&w=majority',
-  cookieKey: '123123123',
-};
