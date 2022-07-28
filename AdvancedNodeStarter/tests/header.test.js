6c76bfab0b5344ec2cb022365eaf3618f4445041
Chris Underwood <cunderwoodmn@gmail.com>
2022-07-27 20:33:25 -0700

    put in before each to get chromium to run before each test.

--- AdvancedNodeStarter/tests/header.test.js
+++ AdvancedNodeStarter/tests/header.test.js
@@ -1,17 +1,16 @@
 const puppeteer = require('puppeteer');
-
-test('Adds two numbers', () => {
-  const sum = 1 + 2;
-  expect(sum).toEqual(3);
-});
-
-test('We can launch a browser', async () => {
-  const browser = await puppeteer.launch({
+let browser;
+let page;
+beforeEach(async () => {
+  browser = await puppeteer.launch({
     headless: false,
     args: ['--no-sandbox'],
   });
-  const page = await browser.newPage();
+  page = await browser.newPage();
   await page.goto('localhost:3000');
+});
+
+test('We can launch a browser', async () => {
   const text = await page.$eval('a.brand-logo', (el) => el.innerHTML);
   expect(text).toEqual('Blogster');
 });
