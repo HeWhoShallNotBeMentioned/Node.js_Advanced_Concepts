f2d1abd9a6f4b1e225509918dfdd1034260bf948
Chris Underwood <cunderwoodmn@gmail.com>
2022-08-24 20:26:45 -0700

    intermediate code for tests.

--- AdvancedNodeStarter/__tests__/header.test.js
+++ AdvancedNodeStarter/__tests__/header.test.js
@@ -22,9 +22,10 @@ test('Confirm the header has the correct text.', async () => {
 });
 
 test('clicking login starts oAuth flow', async () => {
+  await new Promise((r) => setTimeout(r, 2000));
   await page.click('.right a');
   const url = await page.url();
-  //console.log('url', url);
+  console.log('url test 2', url);
   expect(url).toMatch(/accounts.google.com/);
 });
 
@@ -32,11 +33,13 @@ test('When signed in, shows log out button.', async () => {
   const user = await userFactory();
   const { session, sig } = sessionFactory(user);
 
+  await new Promise((r) => setTimeout(r, 2000));
+
   await page.setCookie({ name: 'session', value: session });
   await page.setCookie({ name: 'session.sig', value: sig });
   await page.goto('localhost:3000');
 
-  await page.waitFor('a[href="/auth/logout"]');
+  await page.waitForSelector('a[href="/auth/logout"]');
   const text = await page.$eval('a[href="/auth/logout"]', (el) => {
     return el.innerHTML;
   });
